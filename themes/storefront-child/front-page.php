d2f075fe9661f8376ab0a2091f5d4f099faae6c5
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-10-03 21:15:51 +0200

    pictures in image-folder

--- themes/storefront-child/front-page.php
+++ themes/storefront-child/front-page.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
-								<!--- ACF-lösning för front-page --->
-								<?php if( get_field('hero_image') ): ?>
-    								<img src="<?php the_field('hero_image'); ?>" />
-								<?php endif; ?>
+<!--- ACF-lösning för front-page --->
+<?php if( get_field('hero_image') ): ?>
+    <img src="<?php the_field('hero_image'); ?>" />
+<?php endif; ?>
 								
-								<div class="text">
-									<?php if( get_field('hero_title') ): ?>
-										<h1><?php the_field('hero_title'); ?></h1>
-									<?php endif; ?>
-									<?php if( get_field('hero_text') ): ?>
-										<p><?php the_field('hero_text'); ?></p>
-									<?php endif; ?>
+<div class="text">
+	<?php if( get_field('hero_title') ): ?>
+		<h1><?php the_field('hero_title'); ?></h1>
+	<?php endif; ?>
+	<?php if( get_field('hero_text') ): ?>
+		<p><?php the_field('hero_text'); ?></p>
+	<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
