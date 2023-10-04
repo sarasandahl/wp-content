f355eddc5cc604f53526d531693cb0362918ad8c
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-10-04 20:37:55 +0200

    code cleaning

--- themes/storefront-child/front-page.php
+++ themes/storefront-child/front-page.php
@@ -1,31 +1,30 @@
 <?php get_header(); ?>
 
 <main>
-			<section>
-				<div class="container"> <!--- boostrap --->
-					<div class="row"> <!--- boostrap --->
-						<div class="col-xs-12"> <!--- boostrap --->
-							<div class="hero"> <!--- css --->
+    <section>
+        <div class="container"> <!--- boostrap --->
+            <div class="row"> <!--- boostrap --->
+                <div class="col-xs-12"> <!--- boostrap --->
+                    <div class="hero"> <!--- css --->
+                        <!--- ACF-lösning för front-page --->
+                        <?php if( get_field('hero_image') ): ?>
+                            <img src="<?php the_field('hero_image'); ?>" />
+                        <?php endif; ?>
+                        
+                        <div class="text">
+                            <?php if( get_field('hero_title') ): ?>
+                                <h1><?php the_field('hero_title'); ?></h1>
+                            <?php endif; ?>
+                            <?php if( get_field('hero_text') ): ?>
+                                <p><?php the_field('hero_text'); ?></p>
+                            <?php endif; ?>
+                        </div>
 
-								<!--- ACF-lösning för front-page --->
-								<?php if( get_field('hero_image') ): ?>
-    								<img src="<?php the_field('hero_image'); ?>" />
-								<?php endif; ?>
-								
-								<div class="text">
-									<?php if( get_field('hero_title') ): ?>
-										<h1><?php the_field('hero_title'); ?></h1>
-									<?php endif; ?>
-									<?php if( get_field('hero_text') ): ?>
-										<p><?php the_field('hero_text'); ?></p>
-									<?php endif; ?>
-								</div>
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</main>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
