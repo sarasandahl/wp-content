48eb8b6b4d91a01bd78192e64d811ae34759baf7
sarasandahl <92533105+sarasandahl@users.noreply.github.com>
2023-10-05 10:11:30 +0200

    title in nyheter/index

--- themes/storefront-child/index.php
+++ themes/storefront-child/index.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+            <h1><?php wp_title(''); ?></h1>
 					<!--- the loop för posts i blogg--->
                     <?php 
                         if( have_posts()) {
